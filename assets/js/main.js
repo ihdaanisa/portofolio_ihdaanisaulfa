51028ab6e8b054f3d1d301ea2b7681965a84e561
22091397019IhdaAnisaUlfa <138579358+ihdaanisa@users.noreply.github.com>
2024-06-10 00:07:25 +0700

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -33,7 +33,7 @@ function myMenuFunction(){
 
 /* ----- TYPING EFFECT ----- */
  var typingEffect = new Typed(".typedText",{
-    strings : ["Ihda Anisa Ulfa","Manajemen Informatika"," Mahasiswa Universitas Negeri Surabaya","Developer"],
+    strings : ["Ihda Anisa Ulfa","Mahasiswa Manajemen Informatika"," Mahasiswa Universitas Negeri Surabaya","Developer"],
     loop : true,
     typeSpeed : 100, 
     backSpeed : 80,
